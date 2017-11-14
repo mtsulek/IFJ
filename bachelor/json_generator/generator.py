9dbd2f32ae53b08503a33c35ce766188430bda4a
Mateusz <mt.sulek@gmail.com>
2017-11-14 18:24:25 +0100

    update on json_generator

--- bachelor/json_generator/generator.py
+++ bachelor/json_generator/generator.py
@@ -21,22 +21,13 @@ class myThread (threading.Thread):
         self.coordinateLatitude = random.uniform(0,90)
         self.coordinateLongitude = random.uniform(0,180)
         self.coordinateAltitude = random.uniform(-500,4000)
-<<<<<<< HEAD
-        self.producer = KafkaProducer(bootstrap_servers='192.245.169.38',client_id="python-test-consumer")
-=======
-        self.producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'), bootstrap_servers='192.245.169.38:9092',api_version=(0,10))
->>>>>>> a79f55b2804a062abf2f9f4be3201f3823605f19
+        self.producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'), bootstrap_servers='54.229.196.207:9092',api_version=(0,10))
 
     def run(self):
         while True:
             self.data = {"detectorID": self.threadID, "latitude": self.coordinateLatitude, "longitude": self.coordinateLongitude, "altitude": self.coordinateAltitude, "timestamp": time.time()}
             print(str(self.data) + '\n')
-<<<<<<< HEAD
-            # self.producer.flush(self.data)
-            self.producer.send('foobar',value=b'dane jakies',key=None)
-=======
-            self.producer.send('mateuszTest', value=self.data)
->>>>>>> a79f55b2804a062abf2f9f4be3201f3823605f19
+            self.producer.send('mateuszTest2', value=self.data)
             time.sleep(random.uniform(0.1,1.5))
 
 def createRandomDetector(count):
@@ -58,10 +49,9 @@ def startStreaming(count):
             raise
 
     
-
 # MAIN #####################
 logging.basicConfig(level=logging.INFO)
-numberOfDetectors=5
+numberOfDetectors=1
 createRandomDetector(numberOfDetectors)
 startStreaming(numberOfDetectors)
 
