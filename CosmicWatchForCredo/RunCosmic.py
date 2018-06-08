8de1a058e21e22be797a2cd6ea0f8bee7e75cb5c
MateuszXiaomi <mt.sulek@gmail.com>
2018-06-08 19:45:19 +0200

    update

--- CosmicWatchForCredo/RunCosmic.py
+++ CosmicWatchForCredo/RunCosmic.py
@@ -3,6 +3,7 @@ import requests
 import getpass
 import os
 import platform
+import time
 
 # APPLICATION SETUP
 configFileName = '.CosmicConfig.json'
@@ -13,8 +14,9 @@ device_model = "Xiaomi Air 13"
 device_type = "PC_CosmicWatch"
 device_id = "CosmicWatchTest000000000000001"
 
-print(system_version)
+
 def errors(statusCode, content):
+    """Chcecks for 400 and 500 status code from server"""
     if statusCode == 400 or statusCode == 500:
         print(f"There is {statusCode} status Code from serwer: \"{json.loads(content)['message']}\"")
         return True
@@ -34,18 +36,16 @@ def InitiateCosmicWatch():
     """Guide thruu registering process, returns JSON template for http request"""
     print("No config file detected!")
     print("Register your detector:")
+
     print("email:")
     email = input()
-
     print("username:")
     username = input()
-
     print("displayName:")
     displayName = input()
 
     password = getpass.getpass('password:')
     passwordRepeat = getpass.getpass('repeat your password:')
-
     if password != passwordRepeat:
         print("Passwords are not the same!")
         InitiateCosmicWatch()
@@ -53,7 +53,6 @@ def InitiateCosmicWatch():
 
     print("team:")
     team = input()
-
     print("language:")
     language = input()
 
@@ -68,6 +67,24 @@ def LoginToServer():
     password = getpass.getpass('password:')
     template = JsonTemplate("Login")
     return template(username, password, device_id, device_type, device_model, system_version, app_version)
+
+"""Data Templates"""
+
+def MakeDataFrame(accuracy, altitude, height, id_, latitude, longitude, provider ,timestamp, width):
+    """Generate JSON data frame of detector event"""
+    frame = {
+        "accuracy": accuracy,
+        "altitude": altitude,
+        # "frame_content": pictureFrameContent,
+        "height": height,
+        "id": id_,
+        "latitude": latitude,
+        "longitude": longitude,
+        "provider": provider,
+        "timestamp": timestamp,
+        "width": width
+    }
+    return frame
 
 def JsonTemplate(whichTemplate):
     """get one of parameter: Register, Login, SendData and return template as a function"""
@@ -102,8 +119,8 @@ def JsonTemplate(whichTemplate):
         return template_
 
     def SendData(dataJsonList, device_id, device_type, device_model, system_version, app_version):
-        """Return SendData JSON template"""
-        template = {
+        """Return Data JSON template"""
+        template_ = {
         "detections": [
                 dataJsonList
             ],
@@ -113,12 +130,13 @@ def JsonTemplate(whichTemplate):
             "system_version": system_version,
             "app_version": app_version
         }
+        return template_
 
     if whichTemplate == "Register":
         return Register
     elif whichTemplate == "Login":
         return Login
-    elif whichTemplate == "Login":
+    elif whichTemplate == "Data":
         return SendData
 
 def HttpRequest(IP, whichRequest):
@@ -133,17 +151,19 @@ def HttpRequest(IP, whichRequest):
         _adress = str(IP) + "/api/v2/user/login"
         r = requests.post(_adress, json=dataJSON, verify=False, headers={'Content-Type': 'application/json'})
         return(r.status_code, r.reason, r.content)
-    def SendDataRequest(dataJSON):
+    def SendDataRequest(dataJSON, token):
         """Http SendData request to server"""
         _adress = str(IP) + "/api/v2/detection"
-        r = requests.post(_adress, dataJSON)
+        header = {'Content-Type': 'application/json', 'Authorization': 'Token {}'.format(token)}
+        print(header)
+        r = requests.post(_adress, json=dataJSON, verify=False, headers=header)
         return(r.status_code, r.reason)
 
     if whichRequest == "Register":
         return RegisterRequest
     elif whichRequest == "Login":
         return LoginRequest
-    elif whichRequest == "SendData":
+    elif whichRequest == "Data":
         return SendDataRequest
 
 # Initialization of Detector
@@ -170,4 +190,24 @@ else:
     exit
 AuthenticationToken = json.loads(loginResult[2])['token']
 
+
+
+
+
+
 # Generate data json
+dataTemplate = JsonTemplate("Data")
+dataFrameTemplate = MakeDataFrame(1, 210.73, 1, 1, 50.0922, 19.9148, "gps", int(time.time()), 1)  #DATA FRAME TO SEND
+dataContent = dataTemplate(dataFrameTemplate, device_id, device_type, device_model, system_version, app_version)   #WHOLE DATA TO SEND
+
+# Send Data
+sendRequest = HttpRequest("https://api.credo.science", "Data")
+sendResult = sendRequest(dataContent, AuthenticationToken)
+
+print(dataContent)
+if errors(loginResult[0], loginResult[2]) == False:
+    pass
+else:
+    exit
+
+print(sendResult)
